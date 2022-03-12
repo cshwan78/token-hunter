69e336975a6ab235e4407725d744e995a414d0a0
cshwan78 <cshwan78@gmail.com>
2022-03-13 02:12:26 +0900

    Writing test for parsing github web search result

--- pkg/api/github/main.go
+++ pkg/api/github/main.go
@@ -4,9 +4,19 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
+
+	"github.com/antchfx/htmlquery"
 )
 
-func GetWebSearch(keyword string) (string, error) {
+type searchResult struct {
+	repoName      string
+	filePath      string
+	githubWebPath string
+	snippet       string
+}
+
+func GetWebSearchResult(keyword string) (string, error) {
 	req, err := http.NewRequest("GET", "https://github.com/search?o=desc&s=indexed&type=Code", nil)
 	if err != nil {
 		log.Fatalln(err)
@@ -36,6 +46,51 @@ func GetWebSearch(keyword string) (string, error) {
 	return string(body), nil
 }
 
-func SearchFor(param string) {
+func ParseSearchHTML(htmlbody string) ([]searchResult, error) {
+	doc, err := htmlquery.Parse(strings.NewReader(htmlbody))
 
+	if err != nil {
+		return nil, err
+	}
+
+	var repoListXPath string = "//*[@id='code_search_results']/div[1]/div/div[1]"
+	repoList := htmlquery.Find(doc, repoListXPath)
+	var searchResults []searchResult = make([]searchResult, 0)
+
+	for _, node := range repoList {
+		var snippet string = ""
+
+		repoNameNode := htmlquery.FindOne(node, "/div[1]/a")
+		if repoNameNode == nil {
+			continue
+		}
+		repoName := htmlquery.SelectAttr(repoNameNode, "href")
+
+		filePathNode := htmlquery.FindOne(node, "/div[2]/a")
+		if filePathNode == nil {
+			continue
+		}
+		filePath := htmlquery.SelectAttr(filePathNode, "title")
+		githubWebPath := htmlquery.SelectAttr(filePathNode, "href")
+
+		snippetNode := htmlquery.FindOne(node, "/div[3]")
+		if snippetNode == nil {
+			continue
+		}
+		snippetLinesNodes := htmlquery.Find(node, "/div[3]//td[@class='blob-code blob-code-inner']")
+		for _, lineNode := range snippetLinesNodes {
+			snippet += htmlquery.InnerText(lineNode)
+		}
+		searchResult := searchResult{repoName, filePath, githubWebPath, snippet}
+		searchResults = append(searchResults, searchResult)
+	}
+
+	return searchResults, nil
 }
+
+// func GetSearchResult(keyword string) error {
+// 	htmlbody, err := GetWebSearchResult(keyword)
+// 	if err != nil {
+// 		return nil
+// 	}
+//}
---
+++ test/htmlparse/parse_test.go
@@ -0,0 +1,27 @@
+package test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+
+	"github.com/cshwan78/pkg/api/github"
+)
+
+func TestFoo(t *testing.T) {
+	// todo test code
+	data, err := ioutil.ReadFile("./testsample.html")
+	if err != nil {
+		t.Fatal(err)
+	}
+	htmlbody := string(data)
+
+	result, err := github.ParseSearchHTML(htmlbody)
+	if err != nil {
+
+	}
+	for _, item := range result {
+		fmt.Printf("item: %+v\n", item)
+	}
+
+}
